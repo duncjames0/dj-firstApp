9d137f133f0dfaa66d105a232fa9d8e9a8c53612
Duncan James <duncanjames@Duncans-MBP.broadband>
2022-06-11 14:49:39 +0100

    Added final test for full CI/CD pipeline succcess test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DJ App' });
+  res.render('index', { title: 'DJ App All Gravy' });
 });
 
 module.exports = router;
